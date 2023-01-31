1c7773f8265238cd5e17342a4b7db69fa3cf0a14
Marcela Silva <110864304+Marci-Hime@users.noreply.github.com>
2023-01-31 10:23:00 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Series a1 = new Series (3, 250, 23, "Sword Art Online", 2012, "A-1 Pictures", "Aniplex, Genco, DAX Production, ASCII Media Works, Bandai Namco Games", "Ação, Aventura, Fantasia, Romance", 14, false, true);
+		
+		a1.detalhesSeries();
+	}
+
+}
